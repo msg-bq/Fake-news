f27f40a2dd4db3a29aaf29972e2d4fac5863ac18
msg <82528553+msg-bq@users.noreply.github.com>
2021-09-27 15:06:13 +0800

    Update Pre_concepts.py

--- Knowledge/Pre_concepts.py
+++ Knowledge/Pre_concepts.py
@@ -1,6 +1,8 @@
 from variable_type import *
 
-COVID19_Cure_value = []
+__all__ = ['COVID19_Cure']
+
+COVID19_Cure_value = set()
 COVID19_Cure = Set(value = COVID19_Cure_value,
                            name = 'COVID19_Cure',
                            comments='治疗新冠的药物')
